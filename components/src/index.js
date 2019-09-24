adfefcde084cb737482d77ab7d8e23c4b2a92b41
John McKie <john.mckie@remarkable.net>
2019-09-24 16:38:59 +0100

    Components app started

---
+++ components/src/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" />
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sid
+                    </a>
+                    <div className="metaData">
+                        <span className="date">Today at 6:00pm</span>
+                    </div>
+                    <div className="text">Nice blog post!</div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
