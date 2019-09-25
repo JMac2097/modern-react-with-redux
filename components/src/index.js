8c35ad4903579dd16fdc9f3d7b72af9c4930a2e1
John McKie <john.mckie@remarkable.net>
2019-09-25 16:41:17 +0100

    Some prop action

--- components/src/index.js
+++ components/src/index.js
@@ -1,25 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import CommentDetail from "./CommentDetail";
 
 const App = () => {
-    return (
-        <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" />
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sid
-                    </a>
-                    <div className="metaData">
-                        <span className="date">Today at 6:00pm</span>
-                    </div>
-                    <div className="text">Nice blog post!</div>
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		<div className="ui container comments">
+			<CommentDetail
+				author="Derek"
+				timeAgo="Today at 4:45pm"
+				commentText="Awesome post"
+			/>
+			<CommentDetail
+				author="Arnold"
+				timeAgo="Today at 2:00pm"
+				commentText="Brilliant post"
+			/>
+			<CommentDetail
+				author="Jane"
+				timeAgo="Yesterday at 1:15pm"
+				commentText="Rubbish post"
+			/>
+		</div>
+	);
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
