3ff6bae33c671d2d8151d29401572cf7416b073a
John McKie <john.mckie@remarkable.net>
2019-11-07 14:30:24 +0000

    Axios added and tested

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
-const App = () => {
-    return (
-		<div className="ui container" style={{ marginTop: '10px'}}>
-			<SearchBar />
-		</div>
-	);
-};
+
+
+class App extends React.Component {
+
+	onSearchSubmit(term) {
+		axios.get('https://api.unsplash.com/search/photos', {
+			params: { query: term },
+			headers: {
+				Authorization: 'Client-ID b4fb8a8997cb50b08ca238d6a5001c9821614ad716da8b26f27421e4a45612fe'
+			}
+		});
+	}
+
+
+	render() {
+		return (
+			<div className="ui container" style={{ marginTop: "10px" }}>
+				<SearchBar onSubmit={this.onSearchSubmit} />
+			</div>
+		);
+	}
+}
+
 
 export default App;
--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -4,11 +4,12 @@ class SearchBar extends React.Component {
 
     state = { term: '' };
 
-    onFormSubmit(event) {
+    onFormSubmit = event => {
         event.preventDefault();
 
-        console.log(this.state.term);
-    }
+		this.props.onSubmit(this.state.term);
+		
+    };
 
 
     render() {
